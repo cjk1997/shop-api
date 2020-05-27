5661a1f146aadf4b598b4c319d8b212a97a4696e
Carson Keller <carsonjkeller@gmail.com>
2020-05-27 08:59:02 -0600

    Updated route to accommodate altered filename and additional API file

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const shopRouter = require('./routes/api/shop');
+const shopRouter = require('./routes/api/inventory');
 
 const app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/shop', shopRouter);
+app.use('/api/inventory', shopRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
